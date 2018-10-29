44d911a8685f5e69ec28757a3481101b4ff5bf65
yuqili3 <liyuqi14ab14@gmail.com>
2018-10-28 20:46:45 -0500

    denoise

--- denoising.py
+++ denoising.py
@@ -41,10 +41,10 @@ img_transform = transforms.Compose([
 ])
 
 trainset = noisy_utils.noisy_cifar10(sigma, num_copy=num_copy, dataDir=dataDir, transform=img_transform,train=True)
-trainloader = DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainloader = DataLoader(trainset, batch_size=128, shuffle=True)
 
 testset = noisy_utils.noisy_cifar10(sigma, num_copy=num_copy, dataDir=dataDir, transform=img_transform,train=False)
-testloader = DataLoader(testset, batch_size=100, shuffle=True, num_workers=2)
+testloader = DataLoader(testset, batch_size=100, shuffle=True)
 
 def pairwise_potential(img):
     # TODO: implement sqaure pairwise potential
