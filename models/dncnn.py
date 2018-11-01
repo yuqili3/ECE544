8e9f6cfb771e27b3d51d877d37bd7f04c64df64b
yuqili3 <liyuqi14ab14@gmail.com>
2018-11-01 18:45:57 -0500

    .

--- models/dncnn.py
+++ models/dncnn.py
@@ -3,10 +3,10 @@ import torch
 import torch.nn as nn
 
 cfg = {
-       'CNN16':[17,16,3],
-       'CNN32':[17,32,3],   
-       'CNN64':[17,64,3],
-       'CNN32_5':[17,32,5]
+       'CNN16':[17,16,3,1],
+       'CNN32':[17,32,3,1],   
+       'CNN64':[17,64,3,1],
+       'CNN32_5':[17,32,5,2],
        }
 class deepcnn(nn.Module):
     def __init__(self, name):
@@ -22,18 +22,19 @@ class deepcnn(nn.Module):
         depth = cfg[0] # depth of network
         ch = cfg[1] # number of channels
         k = cfg[2] # kernel size
-        layers += [nn.Conv2d(3,ch,k,padding=1),
+        padding = cfg[3]
+        layers += [nn.Conv2d(3,ch,k,padding=padding),
                   nn.ReLU(True)]
         for i in range(depth-2):
-            layers += [nn.Conv2d(ch,ch,k,padding=1),
+            layers += [nn.Conv2d(ch,ch,k,padding=padding),
                        nn.BatchNorm2d(ch,eps=0.0001, momentum = 0.95),
                        nn.ReLU(True)]
-        layers += [nn.Conv2d(ch,3,k,padding=1)]
+        layers += [nn.Conv2d(ch,3,k,padding=padding)]
         return nn.Sequential(*layers)
     
 def test():
-    net = deepcnn('CNN17')
-    x = torch.randn(2,3,32,32)
+    net = deepcnn('CNN32_5')
+    x = torch.randn(2,3,96,96)
     y = net(x)
     print(y.size())
 #test()
