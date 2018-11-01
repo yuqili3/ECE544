7a24f5ef543fb8a37411cf3f5709f616d17fc127
yuqili3 <liyuqi14ab14@gmail.com>
2018-11-01 18:56:50 -0500

    .

--- utils.py
+++ utils.py
@@ -107,7 +107,7 @@ def PSNR(X):
     psnr = 20*np.log10(np.sqrt(s.prod()) / np.linalg.norm(X))
     return psnr
 
-def denois_example(index,netName='dncnn_CNN32',sigma=0.1,num_copy=1,dataDir='../stl10'):
+def denois_example(index,netName='dncnn_CNN32_5',sigma=0.1,num_copy=1,dataDir='../stl10'):
     testset = dataset.noisy_stl10(sigma, num_copy=num_copy, dataDir=dataDir,train=False)
     noisy = testset.test_data_noisy[index] # range [0,1]
     img = testset.test_data[int(index//num_copy)] # range [0,1]
